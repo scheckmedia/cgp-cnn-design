4adcc3afda14fdabab2da6d48c9d56c833c190b1
Tobias Scheck <tobias@scheck-media.de>
2017-11-27 19:59:36 +0100

    changed parameters

--- run.py
+++ run.py
@@ -9,6 +9,9 @@ def conv_block(**kwargs):
     x = Conv2D(**kwargs)
 
 function_mapping = {
+    'conv_1x1_32_stride_1': {'cls': Conv2D, 'args': {'filters': 32, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
+    'conv_1x1_64_stride_1': {'cls': Conv2D, 'args': {'filters': 64, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
+    'conv_1x1_128_stride_1': {'cls': Conv2D, 'args': {'filters': 128, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'conv_3x3_32_stride_1': {'cls': Conv2D, 'args': {'filters': 32, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'conv_3x3_64_stride_1': {'cls': Conv2D, 'args': {'filters': 64, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'conv_3x3_128_stride_1': {'cls': Conv2D, 'args': {'filters': 128, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
@@ -24,13 +27,11 @@ function_mapping = {
     # 'max_pooling_2x2_2': {'cls': MaxPool2D, 'args': {'pool_size': 2}, 'inputs': 1},
     # 'avg_pooling_2x2_2': {'cls': AveragePooling2D, 'args': {'pool_size': 2}, 'inputs': 1},
     'add': {'cls': Add, 'args': {}, 'inputs': 2},
-    'add_2': {'cls': Add, 'args': {}, 'inputs': 2},
-    'concat_2': {'cls': Concatenate, 'args': {}, 'inputs': 2},
     'concat': {'cls': Concatenate, 'args': {}, 'inputs': 2}
 }
 
 if __name__ == '__main__':
-    trainer = Cifar10Trainer(batch_size=128, epochs=150, verbose=1, lr=[0.1, 0.05, 0.01, 0.005, 0.001])
+    trainer = Cifar10Trainer(batch_size=128, epochs=60, verbose=1, lr=[0.1, 0.05, 0.01])
     e = Evaluator(function_mapping, trainer, input_shape=trainer.input_shape)
     functions, inputs = e.get_function_input_list()
     cfg = CgpConfig(rows=5, cols=30, level_back=10, functions=functions, function_inputs=inputs)
