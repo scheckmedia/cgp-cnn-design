50059c17cafd924d3aed68c686ca48b5ffabac10
Tobias Scheck <tobias@scheck-media.de>
2017-11-28 19:32:33 +0100

    added pointwise convolutions

--- run.py
+++ run.py
@@ -5,23 +5,16 @@ from evaluator.keras_evaluator import Evaluator
 from trainer.cifar_trainer import Cifar10Trainer
 
 
-def conv_block(**kwargs):
-    x = Conv2D(**kwargs)
-
 function_mapping = {
-    'conv_1x1_32_stride_1': {'cls': Conv2D, 'args': {'filters': 32, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
-    'conv_1x1_64_stride_1': {'cls': Conv2D, 'args': {'filters': 64, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
-    'conv_1x1_128_stride_1': {'cls': Conv2D, 'args': {'filters': 128, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
+    'pw_32': {'cls': Conv2D, 'args': {'filters': 32, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
+    'pw_64': {'cls': Conv2D, 'args': {'filters': 64, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
+    'pw_128': {'cls': Conv2D, 'args': {'filters': 128, 'kernel_size': 1, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'conv_3x3_32_stride_1': {'cls': Conv2D, 'args': {'filters': 32, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'conv_3x3_64_stride_1': {'cls': Conv2D, 'args': {'filters': 64, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'conv_3x3_128_stride_1': {'cls': Conv2D, 'args': {'filters': 128, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
-    # 'conv_3x3_256_stride_1': {'cls': Conv2D, 'args': {'filters': 256, 'kernel_size': 3, 'strides': 1, 'padding': 'same'}, 'inputs': 1},
     'dw_3x3_stride_1': {'cls': DepthwiseConv2D, 'args': {'kernel_size': 3, 'padding': 'same', 'strides': 1}, 'inputs': 1},
     'dw_5x5_stride_1': {'cls': DepthwiseConv2D, 'args': {'kernel_size': 5, 'padding': 'same', 'strides': 1}, 'inputs': 1},
     'dw_7x7_stride_1': {'cls': DepthwiseConv2D, 'args': {'kernel_size': 7, 'padding': 'same', 'strides': 1}, 'inputs': 1},
-    #'dwpw_3x3_32_stride_1': {'cls': SeparableConv2D, 'args': {'filters': 32, 'kernel_size': 3, 'strides': 1}, 'inputs': 1},
-    #'dwpw_3x3_64_stride_1': {'cls': SeparableConv2D, 'args': {'filters': 64, 'kernel_size': 3, 'strides': 1}, 'inputs': 1},
-    #'dwpw_3x3_128_stride_1': {'cls': SeparableConv2D, 'args': {'filters': 128, 'kernel_size': 3, 'strides': 1}, 'inputs': 1},
     'max_pooling_2x2': {'cls': MaxPool2D, 'args': {'pool_size': 2}, 'inputs': 1},
     'avg_pooling_2x2': {'cls': AveragePooling2D, 'args': {'pool_size': 2}, 'inputs': 1},
     # 'max_pooling_2x2_2': {'cls': MaxPool2D, 'args': {'pool_size': 2}, 'inputs': 1},
