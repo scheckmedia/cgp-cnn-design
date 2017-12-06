550ecf7ba351ef024c87820502b0f9d080bf0273
Tobias Scheck <tobias@scheck-media.de>
2017-12-06 14:20:27 +0100

    added option to save a confusion matrix

--- fcn_utils/callbacks.py
+++ fcn_utils/callbacks.py
@@ -1,9 +1,10 @@
 from keras.callbacks import Callback
 from fcn_utils.evaluation import calculate_iou
 import numpy as np
+import os
 
 class MeanIoUCallback(Callback):
-    def __init__(self, model, generator, steps, num_classes, every_n_epoch=None, on_end=True):
+    def __init__(self, model, generator, steps, num_classes, every_n_epoch=None, on_end=True, save_path=None):
         self.model = model
         self.generator = generator
         self.steps = steps
@@ -11,15 +12,20 @@ class MeanIoUCallback(Callback):
         self.every_n_epoch = every_n_epoch
         self.on_end = on_end
         self.mean_ious = []
+        self.save_path = save_path
 
     def __calculate(self, logs={}):
-        conf_m, iou, mean_iou = calculate_iou(self.model, self.generator,  self.steps, self.num_classes)
+        conf_matrix, iou, mean_iou = calculate_iou(self.model, self.generator,  self.steps, self.num_classes)
 
-        pixel_acc = np.sum(np.diag(conf_m)) / np.sum(conf_m)
+        pixel_acc = np.sum(np.diag(conf_matrix)) / np.sum(conf_matrix)
         self.mean_ious += [mean_iou]
         logs['mean_iou'] = self.mean_ious[-1]
 
         print("mean IoU: %.4f --- pixel acc: %.4f" % (mean_iou, pixel_acc))
+
+        if self.save_path:
+            np.save(os.path.join(self.save_path, 'conf_matrix'), conf_matrix)
+
         return self.mean_ious[-1]
 
     def on_epoch_end(self, epoch, logs={}):
