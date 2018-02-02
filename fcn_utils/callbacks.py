a3802199851afc73fa6b0322e4145ec54591ecc8
Tobias Scheck <tobias@scheck-media.de>
2018-02-02 19:05:53 +0100

    added eary stop based on mIoU value

--- fcn_utils/callbacks.py
+++ fcn_utils/callbacks.py
@@ -4,7 +4,8 @@ import numpy as np
 import os
 
 class MeanIoUCallback(Callback):
-    def __init__(self, model, generator, steps, num_classes, every_n_epoch=None, on_end=True, save_path=None):
+    def __init__(self, model, generator, steps, num_classes, every_n_epoch=None, on_end=True,
+                 save_path=None, early_stop=None):
         self.model = model
         self.generator = generator
         self.steps = steps
@@ -13,6 +14,7 @@ class MeanIoUCallback(Callback):
         self.on_end = on_end
         self.mean_ious = []
         self.save_path = save_path
+        self.early_stop = early_stop
 
     def __calculate(self, logs={}):
         conf_matrix, iou, mean_iou = calculate_iou(self.model, self.generator,  self.steps, self.num_classes)
@@ -32,10 +34,18 @@ class MeanIoUCallback(Callback):
         if epoch == 0 or self.every_n_epoch == -1 or (self.every_n_epoch is not None and epoch % self.every_n_epoch != 0):
             return
 
-        return self.__calculate(logs)
+        miou = self.__calculate(logs)
+
+        if self.early_stop is not None and epoch in self.early_stop:
+            if self.early_stop[epoch] > miou:
+                self.on_end = False
+                self.model.stop_training = True
+                print("early stop because of bad mIoU")
+
+        return miou
 
     def on_train_end(self, logs=None):
-        if self.on_end == False:
+        if not self.on_end:
             return
 
         return self.__calculate(logs)
